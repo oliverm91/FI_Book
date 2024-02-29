0df0ee6bd07e98535b76d45a056f05ac9d07a3df
oliverm91 <oliver.mohr.b@gmail.com>
2024-02-29 12:04:30 -0300

    .

--- src/tests/main.py
+++ src/tests/main.py
@@ -1,3 +1,6 @@
+from typing import Any
+from json import loads
+
 from OrderBook.OrderBook import FI_Book
 from OrderBook.warning_logger import BookLogger
 
@@ -17,5 +20,14 @@ for fix_message in fix_messages:
         print('\n\t\t'+fix_message.original_message.decode())
         raise e
     
-print(fi_book)
 print('\n\nBook built successfully')
+for book_line in loads(fi_book.to_json()):
+    nemo = book_line[0]
+    sett_condition = book_line[1]
+    orders_data: dict[str, list[dict[str, Any]]] = book_line[2]
+    for side, orders in orders_data.items():
+        for order in orders:
+            notional = order['notional']
+            irr = order['irr']
+            all_amount = order['all_amount']
+            print(f'{side}, {nemo}, {sett_condition}, {notional}, {irr}, {all_amount}')
